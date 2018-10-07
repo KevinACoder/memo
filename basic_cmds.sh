046a6e52c853c57e7cac3a077989c39d70b1c3f9
KevinACoder <38829162+KevinACoder@users.noreply.github.com>
2018-10-07 09:54:38 +0800

    Update basic_cmds.sh

--- basic_cmds.sh
+++ basic_cmds.sh
@@ -1,1 +1,17 @@
 
+#look up process info
+ps -A #select all processes
+pgrep bash  #get pid of process
+cat /proc/$pid/environ | tr '\0' '\n' #get environment variables of the process, with '\0' translated to '\n'
+
+//variables
+var=vaule
+echo $var  #create and print variable
+export PATH="$PATH:/usr/lib" #set the export attribute for variables, add path
+
+//use vim
+#https://www.radford.edu/~mhtay/CPSC120/VIM_Editor_Commands.htm
+a/A/i/I/o/O  #shift from cmd mode to insert mode
+esc #shift from insert mode to cmd mode
+:wq #save and exit
+:set number #show line number
